198855945f020014e37696476de91df9addba4c8
Sienna Newby-Sanchez <snewbz02@gmail.com>
2024-06-14 00:43:56 -0500

    Delete lib directory

--- lib/index.dart
+++
@@ -1,5 +0,0 @@
-// Export pages
-export '/pages/auth3/auth3_widget.dart' show Auth3Widget;
-export '/settings/settings_widget.dart' show SettingsWidget;
-export '/searchzip/searchzip_widget.dart' show SearchzipWidget;
-export '/favorites/favorites_widget.dart' show FavoritesWidget;
